73aa952bf9ced540c830d270ddeec13ea444cb63
Ahmet Enes ADLIĞ <adli_enes@hotmail.com>
2017-07-04 10:06:35 +0300

    Mvc-IlkUygulama
    Stajımın ilk gününde MVC öğrenmek amacıyla geliştirdiğim ilk web tasarım uygulaması

---
+++ MvcIlkProje/MvcIlkProje/Controllers/bize_dairController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcIlkProje.Controllers
+{
+    public class bize_dairController : Controller
+    {
+        // GET: bize_dair
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcIlkProje/MvcIlkProje/Controllers/cozumlerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcIlkProje.Controllers
+{
+    public class cozumlerController : Controller
+    {
+        // GET: cozumler
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
